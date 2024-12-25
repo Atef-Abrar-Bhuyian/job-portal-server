a530a16a5ac3cc4f80c6aa9cbbcb8f3634e2d905
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-12-25 17:41:41 +0600

    jwt token generate fucntion done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 app.use(cors());
 app.use(express.json());
@@ -34,6 +35,13 @@ async function run() {
     const jobApplicationCollection = client
       .db("jobPortal")
       .collection("job_applications");
+
+    // Auth Related APIs
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, "secret", { expiresIn: "1hr" });
+      res.send(token)
+    });
 
     app.get("/jobs", async (req, res) => {
       const email = req.query.email;
